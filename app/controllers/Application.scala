b2e628fb2dce82601ea633cfad0817d1f4c00ee9
Caue Mello <cauemello@gmail.com>
2013-08-03 01:21:04 -0300

    first app

---
+++ app/controllers/Application.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import models.Quote
+
+object Application extends Controller {
+  
+  def index = Action {
+    Ok(views.html.index("Your new application is ready.",Quote("My personal quote","Caue Mello")))
+  }
+  
+}
