485d8e5d8790b08cf5a4fac14667f06b4c9d348c
Caue Mello <cauemello@gmail.com>
2013-08-03 01:29:40 -0300

    added file for heroku

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,9 +5,9 @@ import play.api.mvc._
 import models.Quote
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready.",Quote("My personal quote","Caue Mello")))
+    Ok(views.html.index("Your new application is ready.", Quote("My personal quote", "Somebody")))
   }
-  
+
 }
