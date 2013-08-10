df41f63ea7e122eb1dd606ea6d77fa7433871031
Caue Mello <cauemello@gmail.com>
2013-08-10 02:29:25 -0300

    dependencias

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm,
-    "postgresql" % "postgresql" % "8.4-702.jdbc4"
-    )
+    "postgresql" % "postgresql" % "8.4-702.jdbc4")
 
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
   )
