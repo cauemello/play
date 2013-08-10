24a45dae2fbac0e8a4a6be0a33d4b91e4703d925
Caue Mello <cauemello@gmail.com>
2013-08-10 02:54:06 -0300

    heroku

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName = "tasks app"
+  val appName = "testApp"
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
