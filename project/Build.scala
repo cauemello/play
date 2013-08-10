645f4be08e4cd86a85b9f86490d5330d0eb24e6c
Caue Mello <cauemello@gmail.com>
2013-08-10 02:51:29 -0300

    tentativa heroku

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,9 @@ object ApplicationBuild extends Build {
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "postgresql" % "postgresql" % "8.4-702.jdbc4")
+    jdbc,
+    anorm,
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4")
 
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
   )
